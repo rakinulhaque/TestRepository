06bb6cb2242cbbe52899516a7eb4ebd5a39d50cb
Rakinul haque <rakinulhaque3@gmail.com>
2020-11-10 20:59:53 +0600

    1st branch

--- Hard_task3.py
+++ Hard_task3.py
@@ -1,10 +1,11 @@
-num=int(input())
-count=1
-while(num>1):
-    if(num%2==0):
+num = int(input())
+count = 1
+while(num > 1):
+    if(num % 2 == 0):
         pass
     else:
-        count+=1
-    num=num//2
-number=(2**count)-1
+        count += 1
+    num = num//2
+    # sdddsd
+number = (2**count)-1
 print(number)
